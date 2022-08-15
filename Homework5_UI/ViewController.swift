1fd0615b9e2d51f946d829742e563678c2f81a82
Olena <elenasergeevvnna@gmail.com>
2022-08-15 22:07:23 +0300

    Initial Commit

---
+++ Homework5_UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework5_UI
+//
+//  Created by user on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
