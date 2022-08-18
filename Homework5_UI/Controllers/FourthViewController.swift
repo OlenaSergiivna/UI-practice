be314d2d3f59eb95cf4b9e27e9234d1a75a793ae
Olena <elenasergeevvnna@gmail.com>
2022-08-18 14:46:58 +0300

    rename func

--- Homework5_UI/Controllers/FourthViewController.swift
+++ Homework5_UI/Controllers/FourthViewController.swift
@@ -14,24 +14,24 @@ class FourthViewController: UIViewController {
         
         // MARK: Adding circles
         
-        let circleView = addCenteredCircleProgrammaticalyWithReturn(view: view, centerX: view.bounds.width / 2, centerY: view.bounds.height/2, color: .clear, hwRatio: 2.5)
+        let mainCircleView = addCenteredCircleProgrammaticalyWithReturn(view: view, centerX: view.bounds.width / 2, centerY: view.bounds.height/2, color: .clear, hwRatio: 2.5)
 
 
-        let redCircle = addCenteredCircleProgrammaticalyWithReturn(view: circleView, centerX: circleView.bounds.width / 4, centerY: circleView.bounds.height / 3.3, color: .systemRed, hwRatio: 1.55)
+        let redCircle = addCenteredCircleProgrammaticalyWithReturn(view: mainCircleView, centerX: mainCircleView.bounds.width / 4, centerY: mainCircleView.bounds.height / 3.3, color: .systemRed, hwRatio: 1.55)
         
 
-        let yellowCircle =  addCenteredCircleProgrammaticalyWithReturn(view: circleView, centerX: circleView.bounds.width / (4/3), centerY: circleView.bounds.height / 3.3, color: .systemYellow, hwRatio: 1.55)
+        let yellowCircle =  addCenteredCircleProgrammaticalyWithReturn(view: mainCircleView, centerX: mainCircleView.bounds.width / (4/3), centerY: mainCircleView.bounds.height / 3.3, color: .systemYellow, hwRatio: 1.55)
         
         
-        addCenteredCircleProgrammaticaly(view: circleView, centerX: circleView.bounds.width / (4/3), centerY:  circleView.bounds.width / 1.3, color: .systemGreen, hwRatio: 1.55)
+        addCenteredCircleProgrammaticaly(view: mainCircleView, centerX: mainCircleView.bounds.width / (4/3), centerY:  mainCircleView.bounds.width / 1.3, color: .systemGreen, hwRatio: 1.55)
 
-        addCenteredCircleProgrammaticaly(view: circleView, centerX: circleView.bounds.width / 4, centerY: circleView.bounds.height / 1.3, color: .systemBlue, hwRatio: 1.55)
+        addCenteredCircleProgrammaticaly(view: mainCircleView, centerX: mainCircleView.bounds.width / 4, centerY: mainCircleView.bounds.height / 1.3, color: .systemBlue, hwRatio: 1.55)
         
         // MARK: Adding view settings
         
-        circleView.sendSubviewToBack(yellowCircle)
-        circleView.sendSubviewToBack(redCircle)
-        circleView.clipsToBounds = true
+        mainCircleView.sendSubviewToBack(yellowCircle)
+        mainCircleView.sendSubviewToBack(redCircle)
+        mainCircleView.clipsToBounds = true
   
     }
     
